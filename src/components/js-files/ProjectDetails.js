1b7eb7439bae6b71c60fb2f6f353af5c15d0bff1
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-29 18:53:27 +0530

    JG: update in projects -> add new projects -> remove old projects

--- src/components/js-files/ProjectDetails.js
+++ src/components/js-files/ProjectDetails.js
@@ -1,30 +1,30 @@
 const ProjectDetails = [
    {
       class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/react-ecommerce-clone',
-      projectLink: 'https://react-ecommerce-clone-xi.vercel.app/',
-      title: 'Ecommerce Clone',
+      gitLink: 'https://github.com/Jagrati1213/DesiBazaar',
+      projectLink: 'https://desibazar-xi.vercel.app/',
+      title: 'Desi Bazar',
       des: `It's a react base E- commerce website, where you can add products, delete products, add to wishlist, login, signin`,
       skill: ["TailWind", "React", "Ant-design", "Redux", "LocalStorage"]
    },
    {
       class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
+      gitLink: 'https://github.com/Jagrati1213/Drag-it',
+      projectLink: 'https://react-flow-learning.vercel.app/',
+      title: 'Drag It',
+      des: `Crafted a dynamic project using React Flow `,
+      skill: ["Ant-design", "React", 'React-flow']
+   },
+   {
+      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
       gitLink: 'https://github.com/Jagrati1213/react-spotify-clone',
       projectLink: 'https://react-spotify-clone-one.vercel.app/',
-      title: 'Spotify API Clone',
+      title: 'Spotify Song List Clone',
       des: `It's a spotify clone, where you can check out your's up-to-date songs list`,
       skill: ["TailWind", "React", 'WebAPI', "LocalStorage"]
    },
    {
-      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/react-show-app',
-      projectLink: 'https://react-show-app.vercel.app/',
-      title: 'Movie Book ticket App',
-      des: `It's a react base show's website, where you can login, signin & book tickets for shows. In this project I used Redux to manage state with globall.`,
-      skill: ["TailWind", "React", "Ant-design", "Redux", "LocalStorage"]
-   },
-   {
-      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
+      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
       gitLink: 'https://github.com/Jagrati1213/budget-app',
       projectLink: 'https://jagrati1213.github.io/budget-app',
       title: 'budget-app',
@@ -33,7 +33,7 @@ const ProjectDetails = [
 
    },
    {
-      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
+      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
       gitLink: 'https://github.com/Jagrati1213/snake-game',
       projectLink: 'https://jagrati1213.github.io/snake-game/',
       title: 'Snake Game',
@@ -42,151 +42,37 @@ const ProjectDetails = [
 
    },
    {
-      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
+      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
       gitLink: 'https://github.com/Jagrati1213/Tictacteo',
       projectLink: 'https://jagrati1213.github.io/Tictacteo/',
-      title: 'JS TicTacTeo',
+      title: 'TicTacTeo',
       des: 'It was JS project. Where you can play with your friend.',
       skill: ["html", "JS", "FontAwsome", "Bootstrap"]
    },
    {
-      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/rock-paper-scissor',
-      projectLink: 'https://jagrati1213.github.io/rock-paper-scissor/',
-      title: 'JS Rock Paper Scissor Game',
-      des: 'It was JS project.Here you can play with BOT',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
+      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
       gitLink: 'https://github.com/Jagrati1213/password-generator',
       projectLink: 'https://jagrati1213.github.io/password-generator/',
-      title: 'JS Password Generator',
+      title: 'Password Generator',
       des: 'It was JS project. Where you can generate related Passwords',
       skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/phonepe',
-      projectLink: 'https://jagrati1213.github.io/phonepe/',
-      title: 'Phone Pe Clone',
-      des: 'It was a UPI transition project. A Home page website of original Phonepe. In this project i used Taliwind Css',
-      skill: ["html", "JS", "Tailwind", "AOS", "Slick"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/nike-clone',
-      projectLink: 'https://jagrati1213.github.io/nike-clone/',
-      title: 'Nike Clone',
-      des: 'It was an E-commerce project. A Home page website of original Nike. In this project i used Taliwind Css',
-      skill: ["html", "JS", "Tailwind", "AOS", "Slick"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/ineuron',
-      projectLink: 'https://jagrati1213.github.io/ineuron/',
-      title: 'Ineuron UI Clone',
-      des: 'It was Team mate project for hackathon. here we learnt about slick,aos things',
-      skill: ["html", "JS", "Css", "AOS", "Slick", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/assignment-1',
-      projectLink: 'https://jagrati1213.github.io/assignment-1/',
-      title: 'Protfolio-Home Page UI',
-      des: 'It was an assignment project.A UI dev. profile website',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/assignment-2',
-      projectLink: 'https://jagrati1213.github.io/assignment-2/',
-      title: 'Marketing website UI',
-      des: 'It was an Assignment project. It was business related website',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/assignment-3',
-      projectLink: 'https://jagrati1213.github.io/assignment-3/',
-      title: 'Hosting Website UI',
-      des: 'It was an assignment project. A Home page website of Hosting',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/assignment-4',
-      projectLink: 'https://jagrati1213.github.io/assignment-4/',
-      title: 'API UI',
-      des: 'It was an Assignment project.',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/drumkit',
-      projectLink: 'https://jagrati1213.github.io/drumkit/',
-      title: 'JS DrumKit',
-      des: 'It was a JS project. Here you can play music with help of keys.',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap", "Audio(js)"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/stopwatch',
-      projectLink: 'https://jagrati1213.github.io/stopwatch/',
-      title: 'JS Stop Watch',
-      des: 'It was JS project. here we can stop watch restart watch, start watch.',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap", "SetIntervals"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/js-todolist',
-      projectLink: 'https://jagrati1213.github.io/js-todolist/',
-      title: 'JS To Do List',
-      des: 'It was JS project.Here you can add items,delete items, edit items',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/dad-jokes',
-      projectLink: 'https://jagrati1213.github.io/dad-jokes/',
-      title: 'JS Joke Generator',
-      des: 'It was JS project. Where you can generate related Jokes',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/qr-code-generator',
-      projectLink: 'https://jagrati1213.github.io/qr-code-generator/',
-      title: 'JS QR Code Generator',
-      des: 'It was JS project. Where you can Generate  website QR code',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-   },
-   {
-      class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/shade-up',
-      projectLink: 'https://jagrati1213.github.io/shade-up/',
-      title: 'JS Color Generator',
-      des: 'It was JS project. here we can generate rgb,hex color code.',
-      skill: ["html", "JS", "FontAwsome", "Bootstrap", "SetIntervals"]
    },
    {
       class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
       gitLink: 'https://github.com/Jagrati1213/Quizgame',
       projectLink: 'https://jagrati1213.github.io/Quizgame/',
-      title: 'JS Quizgame',
+      title: 'Quizgame',
       des: 'It was JS project. Where you can ans the related questions',
       skill: ["html", "JS", "FontAwsome", "Bootstrap"]
    },
    {
       class: 'show-more card p-4 cursor-pointer rounded hover:-translate-y-2 transition-all',
-      gitLink: 'https://github.com/Jagrati1213/clock',
-      projectLink: 'https://jagrati1213.github.io/clock/',
-      title: 'JS clock',
-      des: 'It was JS project. Where you can see current Time.',
+      gitLink: 'https://github.com/Jagrati1213/Dictionary',
+      projectLink: 'https://jagrati1213.github.io/Dictionary',
+      title: 'Dictionary App',
+      des: 'Instant definitions, synonyms, and translations in one app.',
       skill: ["html", "JS", "FontAwsome", "Bootstrap"]
-
    },
-
 ]
 
 export default ProjectDetails;
