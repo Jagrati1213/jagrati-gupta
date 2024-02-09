6960528cbe111de9d6505fc7fad5123aa75749ff
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-09 19:25:02 +0530

    JG: optimize code

---
+++ src/components/experience/Experience.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../css-files/Experience.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Education from './Education';
+import Work from './Work';
+
+export default function Experience() {
+
+    AOS.init();
+    return (
+        <>
+            <div className="experience" id="experience" data-aos="fade-up" >
+                <h1 className="heading mb-10">02. Experience</h1>
+                <div className="flex lg:w-4/5 w-full lg:mt-10 md:flex-row flex-col">
+                    <Education />
+                    <Work />
+                </div>
+            </div>
+        </>
+    )
+}
