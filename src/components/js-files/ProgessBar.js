78e14077daab00df70e1a20df39c289d2c5f8522
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-09 19:26:45 +0530

    Jg: make changes

--- src/components/js-files/ProgessBar.js
+++ src/components/js-files/ProgessBar.js
@@ -4,8 +4,10 @@ function ProgessBar({ ProgressText, progressClass }) {
   console.log(progressClass);
   return (
     <>
-      <div className="bg-[#3e4555] w-full h-8 flex my-3">
-        <div className="bg-green-600 text-white px-5 py-1 w-32">{ProgressText}</div>
+      <div className="bg-[#3e4555] w-full h-8 flex my-2">
+        <div className="bg-green-600 text-white text-xs md:text-base flex items-center justify-center w-20 md:w-32">
+          {ProgressText}
+        </div>
         <div data-aos="fade-right" data-aos-duration="1000" data-aos-easing="linear" className='bg-green-500' style={{ width: `${progressClass}%` }}></div>
       </div>
     </>
