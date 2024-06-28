4fd769256e4239c169cff8b24e3c25cd2eae0f9b
Jagrati Gupta <123jagratigupta@gmail.com>
2024-06-29 01:56:22 +0530

    JG: about fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import About from "./components/About/About";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
 import Project from "./components/Project";
-import About from "./components/About/index";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
